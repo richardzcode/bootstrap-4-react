285a60333174917fd1f4cce9b6547dfc17d5fd2d
Richard Zhang <richardzcode@gmail.com>
2018-08-14 09:32:31 -0700

    minor fix

--- src/utilities/display.js
+++ src/utilities/display.js
@@ -15,7 +15,6 @@ export function withDisplay() {
             .map(name => 'd-' + name)
             .concat(typeof visible === 'boolean'? (visible? 'visible' : 'invisible') : [])
         );
-console.log('cn and display', cn, display);
         const p = JS.lessProps(this.props, ['className', 'display', 'visible']);
 
         return <Comp {...p} className={cn} />
