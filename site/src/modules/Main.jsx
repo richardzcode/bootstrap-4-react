cf58db30bd0657d4efe47ca860709c119d10fa86
Richard Zhang <richardzcode@gmail.com>
2018-09-04 13:34:21 -0700

    google analytics

--- site/src/modules/Main.jsx
+++ site/src/modules/Main.jsx
@@ -4,21 +4,40 @@ import Home from './Home';
 import Documentation from './documentation';
 import Example from './Example';
 
+function hitCount(category, label) {
+  if (window.gtag) {
+    const obj = {
+      event_category: category
+    }
+    if (label) { obj.event_label = label; }
+    window.gtag('event', 'hit', obj);
+  }
+}
+
 export default class Main extends Component {
   renderHome(path) {
-    return path === '/'? <Home /> : null;
+    if (path !== '/') { return null; }
+
+    hitCount('home');
+    return <Home />;
   }
 
   renderDocumentation(path) {
     if (!path.startsWith('documentation')) { return null; }
 
-    return <Documentation path={path.replace('documentation', '')}/>
+    const subPath = path.replace('documentation', '');
+
+    hitCount('documentation', subPath);
+    return <Documentation path={subPath}/>
   }
 
   renderExample(path) {
     if (!path.startsWith('example')) { return null; }
 
-    return <Example path={path.replace('example', '')}/>
+    const subPath = path.replace('example', '');
+
+    hitCount('example', subPath);
+    return <Example path={subPath}/>
   }
 
   render() {
