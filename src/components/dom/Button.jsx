becafc29828190805b4e40795072ee7a0e272a26
Richard Zhang <richardzcode@gmail.com>
2018-09-20 10:22:17 -0700

    form and button preventDefault

--- src/components/dom/Button.jsx
+++ src/components/dom/Button.jsx
@@ -3,10 +3,23 @@ import { JS } from 'fsts';
 
 import { stack, domStack } from '../../utilities';
 
+function preventDefaultFn(fn) {
+  return function(event) {
+    fn(event);
+    event.preventDefault();
+  }
+}
+
 class Button extends Component {
   render() {
-    const { htmlDisabled } = this.props;
-    const p = JS.lessProps(this.props, 'htmlDisabled');
+    const { htmlDisabled, preventDefault, onClick } = this.props;
+    const p = JS.lessProps(this.props, ['htmlDisabled', 'preventDefault', 'onClick']);
+
+    if (onClick) {
+      p.onClick = preventDefault
+        ? preventDefaultFn(onClick)
+        : onClick
+    }
 
     return htmlDisabled
       ? <button {...p} disabled>{this.props.children}</button>
--- src/components/dom/Form.jsx
+++ src/components/dom/Form.jsx
@@ -1,10 +1,31 @@
 import React, { Component } from 'react';
+import { JS } from 'fsts';
 
 import { stack, domStack } from '../../utilities';
 
+function preventDefaultFn(fn) {
+  return function(event) {
+    fn(event);
+    event.preventDefault();
+  }
+}
+
 class Form extends Component {
   render() {
-    return <form {...this.props}>{this.props.children}</form>
+    const { preventDefault, onSubmit } = this.props;
+    const p = JS.lessProps(this.props, ['preventDefault', 'onSubmit']);
+
+    if (onSubmit) {
+      p.onSubmit = preventDefault
+        ? preventDefaultFn(onSubmit)
+        : onSubmit
+    } else {
+      if (preventDefault) {
+        p.onSubmit = (event) => { event.preventDefault(); };
+      }
+    }
+
+    return <form {...p}>{this.props.children}</form>
   }
 }
 
